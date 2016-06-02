d9f2e8577bc14e0a75141828d4ca56315784f2f9
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-06-02 10:12:28 +0800

    Add TodoApp component

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,9 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules','./app/components'
+    ],
     alias: {
       applicationStyles: 'app/styles/app.scss'
     },
