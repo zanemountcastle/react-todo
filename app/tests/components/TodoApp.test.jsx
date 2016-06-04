e57648040374226a86068a012097710dfbf080b1
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-06-04 15:11:35 +0800

    Wire up handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,17 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  it('should add todo to todos state on handleAddTodo', () => {
+    var todoText = 'Test text';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({
+      todos: [] //empty todos array
+    });
+
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
