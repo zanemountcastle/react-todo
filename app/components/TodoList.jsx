719d70b0838c0e26c26302ca2ae1770c9804098e
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-06-05 13:59:40 +0800

    Style application

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,6 +5,12 @@ var TodoList = React.createClass({
   render: function(){
     var {todos} = this.props;
     var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className = "container__message">Nothing to do</p>
+        );
+      }
+
       return todos.map((todo) => { //calls function for every element in array
         return(
           //pass every item on todo down as a prop
